7d781da77ae7e71ba2f947507e2ebaf7f36dcc78
Murat Demirci <mdemirci@outlook.com>
2018-07-31 17:08:48 +0300

    contact form ajax

--- CommerceV3/Controllers/HomeController.cs
+++ CommerceV3/Controllers/HomeController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using CommerceV3.Models;
 using CommerceV3.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Net.Mail;
+using System.Net;
 
 namespace CommerceV3.Controllers
 {
@@ -35,9 +38,37 @@ namespace CommerceV3.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
-
+            ViewBag.Countries = new SelectList(db.Regions.Where(r => r.RegionType == RegionType.Country).OrderBy(o => o.Name).ToList(),"Id","Name");
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(string fullName, string email, string country, string city, string subject, string message)
+        {
+            // mail gönderme işlemi
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential("denemecvhavuzu@gmail.com", "123:Asdfg");
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("denemecvhavuzu@gmail.com");
+            mailMessage.To.Add("murat.demirci@bilisimegitim.com");
+
+            // ülke ve şehir adlarını al
+            var countryName = db.Regions.FirstOrDefault(r => r.Id == country)?.Name;
+            var cityName = db.Regions.FirstOrDefault(r => r.Id == city)?.Name;
+
+            mailMessage.Body = "Tam Ad: " + fullName + "\nE-posta: " + email + "\nÜlke: " + countryName + "\nŞehir: " + cityName + "\nMesaj: " + message;
+            mailMessage.Subject = subject;
+            client.Send(mailMessage);
+            ViewBag.Message = "Mesajınız başarıyla iletilmiştir, en kısa sürede dönüş yapılacaktır.";
+            return View();
+        }
+
+        public IEnumerable<Region> GetCities(string parentRegionId)
+        {
+            return db.Regions.Where(r => r.RegionType == RegionType.City && r.ParentRegionId == parentRegionId).OrderBy(o => o.Name).ToList();
         }
 
         public IActionResult Privacy()
